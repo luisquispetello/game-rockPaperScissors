e328122575d789915556b1bd4c37d0248fecc3f5
Luis Quispe Tello <luisquispetello@gmail.com>
2022-05-09 13:22:10 +0200

    updated CSS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 let userScore = 0
 let cpuScore = 0
 
-let $ = document.getElementById
-
-const userScoreCount = $('score-user')
-const cpuScoreCount = $('score-cpu')
-const rematchBtn = $('rematch-btn')
-const choiceRock = $('rock')
-const choicePaper = $('paper')
-const choiceScissors = $('scissors')
-const resultBox = $('result-box')
-const resultText = $('result-box-content')
+const userScoreCount = document.getElementById('score-user')
+const cpuScoreCount = document.getElementById('score-cpu')
+const rematchBtn = document.getElementById('rematch-btn')
+const choiceRock = document.getElementById('rock')
+const choicePaper = document.getElementById('paper')
+const choiceScissors = document.getElementById('scissors')
+const resultBox = document.getElementById('result-box')
+const resultText = document.getElementById('result-box-content')
 
 function getCpuChoice() {
     const choices = ['rock', 'paper','scissors']
@@ -21,16 +19,16 @@ function getCpuChoice() {
 function win(userChoice, cpuChoice){
     userScore++
     userScoreCount.innerText = userScore
-    $(userChoice).classList.add('green-win')
-    setTimeout(() => $(userChoice).classList.remove('green-win'), 2500)
+    document.getElementById(userChoice).classList.add('green-win')
+    setTimeout(() => document.getElementById(userChoice).classList.remove('green-win'), 2500)
     resultText.innerHTML = `<h1>You win!</h1> ${userChoice} smash ${cpuChoice}`
     resultBox.style.display = 'grid'
     setTimeout(() => resultBox.style.display = 'none', 2500) 
 }
 
 function draw(userChoice){
-    $(userChoice).classList.add('grey-draw')
-    setTimeout(() => $(userChoice).classList.remove('grey-draw'), 2500)
+    document.getElementById(userChoice).classList.add('grey-draw')
+    setTimeout(() => document.getElementById(userChoice).classList.remove('grey-draw'), 2500)
     resultText.innerHTML = `<h1>Draw</h1> You both chose ${userChoice}`
     resultBox.style.display = 'grid'
     setTimeout(() => resultBox.style.display = 'none', 2500) 
@@ -39,8 +37,8 @@ function draw(userChoice){
 function lose(userChoice, cpuChoice){
     cpuScore++
     cpuScoreCount.innerText = cpuScore
-    $(userChoice).classList.add('red-lose')
-    setTimeout(() => $(userChoice).classList.remove('red-lose'), 2500)
+    document.getElementById(userChoice).classList.add('red-lose')
+    setTimeout(() => document.getElementById(userChoice).classList.remove('red-lose'), 2500)
     resultText.innerHTML = `<h1>You lost</h1> ${cpuChoice} smash ${userChoice}`
     resultBox.style.display = 'grid'
     setTimeout(() => resultBox.style.display = 'none', 2500) 
